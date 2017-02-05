181b40cda9de02ed332460b4891bda1a5542ad7d
A bei's mac <Bei1515852657>
2017-02-05 20:17:37 +0800

    first commit

---
+++ src/common/js/publicVue.js
@@ -0,0 +1,6 @@
+/**
+ * Created by abeismac on 2016/12/23.
+ */
+var Vue = require('vue');
+
+export default new Vue();
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App.vue';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+import 'common/stylus/index.styl';
+Vue.use(VueRouter);
+Vue.use(VueResource);
+/* eslint-disable no-new */
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active',
+  routes: [
+    {path: '/goods', component: goods},
+    {path: '/ratings', component: ratings},
+    {path: '/seller', component: seller}
+  ]
+});
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(app)
+});
+// router.push('/goods');
